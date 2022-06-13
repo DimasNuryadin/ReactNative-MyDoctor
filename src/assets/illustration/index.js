616ddd1fc6086189e0f49f6545cfb0ceb5457c7b
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2022-06-13 11:31:18 +0700

    Merapikan struktur folder, membuat pages splash screen dan Getstarted

---
+++ src/assets/illustration/index.js
@@ -0,0 +1,5 @@
+// import illustration
+import ILLogo from './logo.svg';
+import ILGetStarted from './get-started.png';
+
+export { ILLogo, ILGetStarted };
---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+// Akan mengeksport seluruh assets
+// export * from './dummy';
+// export * from './icon';
+export * from './illustration';
