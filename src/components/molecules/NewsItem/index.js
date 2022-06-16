7b4948ab89625e214d9e4e11cc5a4a080cc7ef5a
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2022-06-16 22:45:20 +0700

    Finish slicing doctor page

--- src/components/molecules/NewsItem/index.js
+++ src/components/molecules/NewsItem/index.js
@@ -1,12 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { DummyNews1 } from '../../../assets';
+import { colors, fonts } from '../../../utils';
 
 export default function NewsItems() {
   return (
-    <View>
-      <Text>NewsItems</Text>
+    <View style={styles.container}>
+      <View style={styles.titleWrapper}>
+        <Text style={styles.title}>
+          Is it safe to stay at home during coronavirus?
+        </Text>
+        <Text style={styles.date}>Today</Text>
+      </View>
+      <Image source={DummyNews1} style={styles.image} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderColor: colors.border,
+    paddingBottom: 12,
+    paddingTop: 16,
+  },
+  titleWrapper: {
+    flex: 1,
+  },
+  title: {
+    color: colors.text.primary,
+    fontFamily: fonts.primary[600],
+    fontSize: 16,
+    marginBottom: 4,
+    maxWidth: '94%',
+  },
+  date: {
+    color: colors.text.secondary,
+    fontFamily: fonts.primary.normal,
+    fontSize: 12,
+  },
+  image: {
+    height: 60,
+    width: 80,
+    borderRadius: 11,
+  },
+});
--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -14,33 +14,38 @@ export default function Doctor() {
     <View style={styles.page}>
       <View style={styles.content}>
         <ScrollView showsVerticalScrollIndicator={false}>
-          <Gap height={30} />
-          <HomeProfile />
-          <Text style={styles.welcome}>
-            Mau konsultasi dengan siapa hari ini?
-          </Text>
+          <View style={styles.wrapperSection}>
+            <Gap height={30} />
+            <HomeProfile />
+            <Text style={styles.welcome}>
+              Mau konsultasi dengan siapa hari ini?
+            </Text>
+          </View>
+
           <View style={styles.wrapperScroll}>
             <ScrollView horizontal showsHorizontalScrollIndicator={false}>
               <View style={styles.category}>
-                <Gap width={16} />
+                <Gap width={32} />
                 <DoctorCategory />
                 <DoctorCategory />
                 <DoctorCategory />
                 <DoctorCategory />
                 <DoctorCategory />
                 <DoctorCategory />
-                <Gap width={6} />
+                <Gap width={22} />
               </View>
             </ScrollView>
           </View>
-          <Text style={styles.sectionLabel}>Top Rated Doctors</Text>
-          <RatedDoctor />
-          <RatedDoctor />
-          <RatedDoctor />
-          <Text style={styles.sectionLabel}>Good News</Text>
-          <NewsItems />
-          <NewsItems />
-          <NewsItems />
+          <View style={styles.wrapperSection}>
+            <Text style={styles.sectionLabel}>Top Rated Doctors</Text>
+            <RatedDoctor />
+            <RatedDoctor />
+            <RatedDoctor />
+            <Text style={styles.sectionLabel}>Good News</Text>
+            <NewsItems />
+            <NewsItems />
+            <NewsItems />
+          </View>
           <Gap height={30} />
         </ScrollView>
       </View>
@@ -54,7 +59,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   content: {
-    paddingHorizontal: 16,
     backgroundColor: colors.white,
     flex: 1,
     borderBottomLeftRadius: 20,
@@ -73,6 +77,9 @@ const styles = StyleSheet.create({
   },
   wrapperScroll: {
     marginHorizontal: -16,
+  },
+  wrapperSection: {
+    paddingHorizontal: 16,
   },
   sectionLabel: {
     fontSize: 16,
