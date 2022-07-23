f06eb6401b07d93e648f3446fcd9034f5ecb31d7
Dimas Nuryadin <dimasnuryadinn@gmail.com>
2022-07-23 21:03:40 +0700

    Menampilkan history chat

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,41 +1,55 @@
 import { StyleSheet, Text, View } from 'react-native';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { List } from '../../components';
-import { colors, fonts } from '../../utils';
-import { DummyDoctor1, DummyDoctor2, DummyDoctor3 } from '../../assets';
+import { colors, fonts, getData } from '../../utils';
+
+// Firebase
+import '../../config';
+import { getDatabase, ref, onValue } from 'firebase/database';
+const db = getDatabase();
 
 export default function Messages({ navigation }) {
-  const [doctors] = useState([
-    {
-      id: 1,
-      profile: DummyDoctor1,
-      name: 'Alexander Jannie',
-      desc: 'Baik ibu, terima kasih banyak atas wakt...',
-    },
-    {
-      id: 2,
-      profile: DummyDoctor2,
-      name: 'Nairobi Putri Hayza',
-      desc: 'Oh tentu saja tidak karena jeruk it...',
-    },
-    {
-      id: 3,
-      profile: DummyDoctor3,
-      name: 'John McParker Steve',
-      desc: 'Oke menurut pak dokter bagaimana unt...',
-    },
-  ]);
+  const [user, setUser] = useState([]);
+  const [historyChat, setHistoryChat] = useState([]);
+
+  useEffect(() => {
+    getDataUserFromLocal();
+    const urlHistory = `messages/${user.uid}/`;
+    onValue(ref(db, urlHistory), snapshot => {
+      // console.log('data history :', snapshot.val());
+      if (snapshot.val()) {
+        const oldData = snapshot.val();
+        const data = [];
+        Object.keys(oldData).map(key => {
+          data.push({
+            id: key,
+            ...oldData[key],
+          });
+        });
+        console.log('data baru', data);
+        setHistoryChat(data);
+      }
+    });
+  }, [user.uid]);
+
+  const getDataUserFromLocal = () => {
+    getData('user').then(res => {
+      // console.log('user login', res);
+      setUser(res);
+    });
+  };
+
   return (
     <View style={styles.page}>
       <View style={styles.content}>
         <Text style={styles.title}>Messages</Text>
-        {doctors.map(doctor => {
+        {historyChat.map(chat => {
           return (
             <List
-              key={doctor.id}
-              profile={doctor.profile}
-              name={doctor.name}
-              desc={doctor.desc}
+              key={chat.id}
+              profile={chat.uidPartner}
+              name={chat.uidPartner}
+              desc={chat.lastContentChat}
               onPress={() => navigation.navigate('Chatting')}
             />
           );
